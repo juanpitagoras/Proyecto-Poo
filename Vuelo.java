127509dd073db6d4e1e401d3b185ec7089d335a2
Sampg1012 <sportog@unicartagena.edu.co>
2026-05-24 16:59:08 -0500

    Add Vuelo class with flight attributes

---
+++ Vuelo.java
@@ -0,0 +1,15 @@
+public abstract class Vuelo {
+    protected String id;
+    protected String origen;
+    protected String destino;
+    protected String fechaHoraSalida;
+    protected String fechaHoraLlegada;
+    protected double duracion;
+    protected double tarifaBase;
+    protected String estado;
+    protected boolean esDirecto;
+    
+    // falta agregar atributos para asietos y pasajeros
+
+    
+}
